74b089473630a58f24ee8e539a6b35e7773cb0f8
RODRIGO GONCALVES YUNOGUTHI <RGYUNOGU@trf3.jus.br>
2019-02-01 14:31:44 -0200

    traduzindo

---
+++ app/Notifications/DefaultResetPasswordNotification.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Notifications\Notification;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+
+class DefaultResetPasswordNotification extends ResetPassword
+{
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        return (new MailMessage)
+                    ->line('The introduction to the notification.')
+                    ->action('Notification Action', url('/'))
+                    ->line('Thank you for using our application!');
+    }
+
+    /**
+     * Build the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        return (new MailMessage)
+            ->subject('Redefinição de senha')
+            ->line('Você está recebendo este e-mail, porque uma redefinição de senha foi requisitada.')
+            ->action('Redefinir senha', url(config('app.url').route('password.reset', $this->token, false)))
+            ->line('Se você não requisitou isto, por favor desconsidere.');
+    }
+
+}
--- app/User.php
+++ app/User.php
@@ -30,4 +30,10 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    public function sendPasswordResetNotification($token)
+    {
+        $this->notify(new DefaultResetPasswordNotification($token));
+    }
+
 }
